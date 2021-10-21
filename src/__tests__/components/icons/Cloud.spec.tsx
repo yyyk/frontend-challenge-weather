2e190fc1d9d27401b6e31d4919c95bd39173c713
Yuki Kishino <u.key1203@gmail.com>
2021-10-21 19:33:24 +0200

    Update tests for Sun and Cloud components

--- src/__tests__/components/icons/Cloud.spec.tsx
+++ src/__tests__/components/icons/Cloud.spec.tsx
@@ -1,9 +1,14 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Cloud from 'src/components/icons/Cloud';
 
 describe('<Cloud>', () => {
   it('matches snapshot', () => {
     const { asFragment } = render(<Cloud />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('has title tag', () => {
+    render(<Cloud />);
+    expect(screen.getByText('Cloudy', { selector: 'title' })).toBeTruthy();
   });
 });
--- src/__tests__/components/icons/Sun.spec.tsx
+++ src/__tests__/components/icons/Sun.spec.tsx
@@ -1,9 +1,14 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Sun from 'src/components/icons/Sun';
 
 describe('<Sun>', () => {
   it('matches snapshot', () => {
     const { asFragment } = render(<Sun />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('has title tag', () => {
+    render(<Sun />);
+    expect(screen.getByText('Clear', { selector: 'title' })).toBeTruthy();
   });
 });
