ebcea96fe73c586b3e083ee96b56592593d40687
Yuki Kishino <u.key1203@gmail.com>
2021-10-21 19:35:37 +0200

    Add colors constants file for styling

---
+++ src/constants/colors.ts
@@ -0,0 +1,14 @@
+const black = '#000000';
+const white = '#ffffff';
+
+const text = {
+  gray: '#a8aabd',
+};
+
+const background = {
+  darkBlue: '#262a59',
+};
+
+const colors = { black, white, text, background };
+
+export default colors;
