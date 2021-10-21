c2e8254705c5bc6007301f2255ec7ecbe8dc3288
Yuki Kishino <u.key1203@gmail.com>
2021-10-21 20:29:46 +0200

    Update App.tsx and Delete App.css and logo.svg

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('src/pages/Index', () => () => <div data-testid="index" />);
+
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByTestId(/index/)).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SelectedWeatherProvider from 'src/providers/SelectedWeather';
+import IndexPage from 'src/pages/Index';
+import 'src/styles/reset.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SelectedWeatherProvider>
+      <IndexPage />
+    </SelectedWeatherProvider>
   );
 }
 
