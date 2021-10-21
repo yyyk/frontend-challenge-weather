f3a28511cdd62e2f84d2ad07fa836037af52cf3c
Yuki Kishino <u.key1203@gmail.com>
2021-10-21 19:26:03 +0200

    Delete dateList.ts and Add dayList.ts and monthList.ts

--- src/constants/dateList.ts
+++
@@ -1,1 +0,0 @@
-export const DATE_LIST = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
---
+++ src/constants/dayList.ts
@@ -0,0 +1,1 @@
+export const DAY_LIST = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
